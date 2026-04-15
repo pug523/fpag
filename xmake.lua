e6fc2185e8eda0c108bad6f6e9c5a3af943a3870
pugur <pugurmc@gmail.com>
2026-04-15 19:45:32 +0900

    build: fix windows build fails since it has no --build-id flag

--- xmake.lua
+++ xmake.lua
@@ -180,7 +180,9 @@ target("fpag.root_config")
     set_symbols("debug", { public = true })
     set_optimize("none", { public = true })
     add_cxxflags("-fno-omit-frame-pointer", "-g3", { public = true })
-    add_ldflags("-Wl,--build-id", { public = true })
+    if not is_plat("windows") then
+      add_ldflags("-Wl,--build-id", { public = true })
+    end
     add_defines("LLVM_ENABLE_STATS", "LLVM_ENABLE_DUMP", { public = true })
   elseif is_mode("release") then
     set_symbols("hidden", { public = true })
