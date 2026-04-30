135265f6ee86300b16cc32986284bb322b88327b
pugur <pugurmc@gmail.com>
2026-05-01 01:26:49 +0900

    refactor: rename all benchmarks to having _bench suffix

--- benchmarks/async_logger_bench.cc
+++ benchmarks/async_logger_bench.cc
@@ -2,10 +2,11 @@
 // This source code is licensed under the Apache License, Version 2.0
 // which can be found in the LICENSE file.
 
-#include "fpag/logging/async/async_logger.h"
-
 #include "benchmark/benchmark.h"
+#include "fmt/compile.h"
 #include "fpag/base/numeric.h"
+#include "fpag/base/spsc_queue.h"
+#include "fpag/logging/async/async_logger.h"
 #include "fpag/logging/log_level.h"
 #include "fpag/logging/sink/null_sink.h"
 
@@ -15,15 +16,15 @@ namespace {
 
 // NOLINTBEGIN(clang-analyzer-deadcode.DeadStores)
 void async_logger_log_literal_string(benchmark::State& state) {
-  const u64 capacity = 1 << 20;
-  const u32 num_ops = 100000;
+  const usize map_capacity = 1 << 16;
+  const usize queue_capacity = 1 << 20;
   AsyncLogger<NullSink, LogLevel::Info> logger;
-  logger.init({}, capacity);
+  logger.init({}, map_capacity, queue_capacity, base::SpscQueue::Mode::kDrop);
   logger.start_backend_worker();
+  // usize i = 0;
   for (auto _ : state) {
-    for (u32 i = 0; i < num_ops; ++i) {
-      logger.info("test");
-    }
+    logger.info(FMT_COMPILE("test"));
+    // }
   }
   logger.stop_backend_worker();
 }
