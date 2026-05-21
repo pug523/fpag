ade17fb06541a3979adf8e8eb1a1cdd78e1f3b6c
pugur <pugurmc@gmail.com>
2026-05-21 22:12:33 +0900

    feat: add i128, u128, isize

--- include/fpag/base/numeric.h
+++ include/fpag/base/numeric.h
@@ -7,16 +7,35 @@
 #include <cstddef>
 #include <cstdint>
 
+#include "fpag/build/build_config.h"
+
 using i8 = int8_t;
 using i16 = int16_t;
 using i32 = int32_t;
 using i64 = int64_t;
+using i128 = __int128_t;
 
 using u8 = uint8_t;
 using u16 = uint16_t;
 using u32 = uint32_t;
 using u64 = uint64_t;
+using u128 = __uint128_t;
 using usize = size_t;
 
 using f32 = float;
 using f64 = double;
+
+#if FPAG_BUILD_FLAG(IS_OS_POSIX)
+#include <unistd.h>
+using isize = ssize_t;
+#elif FPAG_BUILD_FLAG(IS_OS_WIN)
+#if FPAG_BUILD_FLAG(ARCH_CPU_64_BITS)
+using isize = i64;
+#elif FPAG_BUILD_FLAG(ARCH_CPU_32_BITS)
+using isize = i32;
+#else
+#error "Unsupported cpu architecture"
+#endif
+#else
+#error "Unsupported os"
+#endif
