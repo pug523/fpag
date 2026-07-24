9c945b6cd1b5a4425d2f272bef8bf8e037716983
pugur <pugurmc@gmail.com>
2026-07-24 12:06:09 +0900

    feat: add color mode <-> str conversion helper

--- include/fpag/base/color_mode.h
+++ include/fpag/base/color_mode.h
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include <string_view>
+
 #include "fpag/base/numeric.h"
 
 namespace base {
@@ -12,7 +14,29 @@ enum class ColorMode : u8 {
   Auto,
   Always,
   Never,
+  Unknown,
 };
+
+inline constexpr ColorMode str_to_color_mode(const std::string_view s) {
+  if (s == "auto") {
+    return ColorMode::Auto;
+  } else if (s == "always") {
+    return ColorMode::Always;
+  } else if (s == "never") {
+    return ColorMode::Never;
+  } else {
+    return ColorMode::Unknown;
+  }
+}
+
+inline constexpr const char* color_mode_to_str(ColorMode c) {
+  switch (c) {
+    case ColorMode::Auto: return "auto";
+    case ColorMode::Always: return "always";
+    case ColorMode::Never: return "never";
+    default: return "unknown";
+  }
+}
 
 }  // namespace base
 
